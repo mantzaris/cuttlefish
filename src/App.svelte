26da0895d8596e58c3abc7d8870799f8b0706f0d
mantzaris <avmantzaris@gmail.com>
2023-11-10 19:42:33 -0500

    menubar skeleton placement and components without expansion accordion style yet

--- src/App.svelte
+++ src/App.svelte
@@ -4,72 +4,50 @@
   import Button, { Label } from '@smui/button'
   import LayoutGrid, { Cell } from '@smui/layout-grid'
 
-  let count = 0
-  onMount(() => {
-    const interval = setInterval(() => count++, 1000)
-    return () => clearInterval(interval)
-  })
+
 </script>
 
-<div id="main-page">
+<body>
+
   <div id="menu">
     <MenuBar />
   </div>
 
   <LayoutGrid>
-    <Cell span={12} style="height: 25vh;">
-      <div class="demo-cell">fdgsdfhg</div>
+    <Cell span={12}>
+      <div class="cell-component">Content of the first row</div>
     </Cell>
-    <Cell span={12} style="height: 25vh;">
-      <div class="demo-cell">fdhtrtyf445355</div>
+    <Cell span={12} >
+      <div class="cell-component">Content of the 2 row</div>
     </Cell>
-    <Cell span={12} style="height: 25vh;">
-      <div class="demo-cell">4545fghfd</div>
+    <Cell span={12} >
+      <div class="cell-component">Content of the 3 row</div>
+    </Cell>
+    <Cell span={12} >
+      <div class="cell-component">Content of the 4 row</div>
+    </Cell>
+    <Cell span={12} >
+      <div class="cell-component">Content of the 5 row</div>
     </Cell>
   </LayoutGrid>
 
-  <div id="components">
-    <div class="utility">hello1</div>
-
-    <Button touch variant="raised">
-      <Label>Raised</Label>
-    </Button>
-
-    <h1>cuttlefish</h1>
-    <p>Edit <code>src/App.svelte</code> to trigger HMR</p>
-    <p>Counter: <code>{count}</code></p>
-  </div>
-</div>
+</body>
 
 <style lang="sass">
-  .demo-cell
-    width: 100%
-    background-color: red
   
-
-  #main-page
-    display: flex
-    flex-direction: column
-    height: 100vh
-    width: 100vw
+  body
+    overflow: hidden
+    height: 100%
     margin: 0
     padding: 0
 
   #menu
     height: 10vh
-
-  #components
-    flex: 1
-
-  .utility
+  
+  .cell-component
     height: 25vh
-    background-color: lightgray  
+    margin-top: 5px
+    background-color: brown
 
-  h1
-    color: #red
-  code
-    background: #282b33
-    padding: 2px 5px
-    border-radius: 4px
 
 </style>
--- src/components/MenuBar.svelte
+++ src/components/MenuBar.svelte
@@ -2,15 +2,15 @@
   // You can add your JavaScript or TypeScript here
 </script>
 
-<div id="menu-bar">menu bar yo</div>
+<div id="menu-bar">menu bar </div>
 
 <style lang="sass">
     #menu-bar
+      background-color: blue
       width: 100%
       height: 100%
-      background-color: black 
-      color: white
-      padding: 0
-      margin: 0
-
+      margin-bottom: 5px
+      color: #ecf0f1
+      text-align: center
+      
 </style>
